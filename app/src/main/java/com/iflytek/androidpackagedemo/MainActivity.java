5dff318ed2bd28e1ce7719f0f5ba0cef1cbae979
mhlei <mhlei@iflytek.com>
2019-04-21 11:25:52 +0800

    flavor测试

---
+++ app/src/main/java/com/iflytek/androidpackagedemo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.iflytek.androidpackagedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String packageName = getPackageName();
+        Log.e("lemon", "package name = " + packageName);
+    }
+}
